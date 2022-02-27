25590b988c992b53e31754ed49536143d58dd198
finlaywashere <finman292004@protonmail.com>
2022-02-27 17:41:04 -0500

    Moved functions from master.js to inventory subsystem

--- assets/js/master.js
+++ assets/js/master.js
@@ -1,18 +1,18 @@
 // From https://stackoverflow.com/a/5448635
 function getSearchParameters() {
-	var prmstr = window.location.search.substr(1);
-	return prmstr != null && prmstr != "" ? transformToAssocArray(prmstr) : {};
+    var prmstr = window.location.search.substr(1);
+    return prmstr != null && prmstr != "" ? transformToAssocArray(prmstr) : {};
+}
+function transformToAssocArray( prmstr ) {
+    var params = {};
+    var prmarr = prmstr.split("&");
+    for ( var i = 0; i < prmarr.length; i++) {
+        var tmparr = prmarr[i].split("=");
+        params[tmparr[0]] = tmparr[1];
+    }
+    return params;
 }
 
-function transformToAssocArray( prmstr ) {
-	var params = {};
-	var prmarr = prmstr.split("&");
-	for ( var i = 0; i < prmarr.length; i++) {
-		var tmparr = prmarr[i].split("=");
-		params[tmparr[0]] = tmparr[1];
-	}
-	return params;
-}
 function clearTable(t){
 	var children = t.querySelectorAll('tr')
 	for(let i = 0; i < children.length; i++){
